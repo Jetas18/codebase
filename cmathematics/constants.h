609093824c5ccac2b63fdc1e15da94f085f9d3e6
Jetas18 <tejasa070@gmail.com>
2023-12-12 11:27:07 +0530

    Constants added

--- cmathematics/constants.h
+++ cmathematics/constants.h
@@ -10,4 +10,70 @@ extern const F64 pi_f64;
 extern const F32 gold_f32;
 extern const F64 gold_f64;
 
+extern const F32 gravity_constant_f32;
+extern const F64 gravity_constant_f64;
+
+extern const F32 light_speed_f32;
+extern const F64 light_speed_f64;
+
+extern const F32 avogadro_constant_f32;
+extern const F64 avogadro_constant_f64;
+
+extern const F32 electron_charge_f32;
+extern const F64 electron_charge_f64;
+
+extern const F32 boltzmann_constant_f32;
+extern const F64 boltzmann_constant_f64;
+
+extern const F32 planck_constant_f32;
+extern const F64 planck_constant_f64;
+
+extern const F32 mars_gravity_f32;
+extern const F64 mars_gravity_f64;
+
+extern const F32 earth_gravity_f32;
+extern const F64 earth_gravity_f64;
+
+extern const F32 moon_gravity_f32;
+extern const F64 moon_gravity_f64;
+
+extern const F32 sun_gravity_f32;
+extern const F64 sun_gravity_f64;
+
+extern const F32 jupiter_gravity_f32;
+extern const F64 jupiter_gravity_f64;
+
+extern const F32 saturn_gravity_f32;
+extern const F64 saturn_gravity_f64;
+
+extern const F32 uranus_gravity_f32;
+extern const F64 uranus_gravity_f64;
+
+extern const F32 neptune_gravity_f32;
+extern const F64 neptune_gravity_f64;
+
+extern const F32 mercury_gravity_f32;
+extern const F64 mercury_gravity_f64;
+
+extern const F32 venus_gravity_f32;
+extern const F64 venus_gravity_f64;
+
+extern const F32 finestructure_constant_f32;
+extern const F64 finestructure_constant_f64;
+
+extern const F32 rydberg_constant_f32;
+extern const F64 rydberg_constant_f64;
+
+extern const F32 bohr_magneton_f32;
+extern const F64 bohr_magneton_f64;
+
+extern const F32 vaccum_magnetic_permeability_f32;
+extern const F64 vaccum_magnetic_permeability_f64;
+
+extern const F32 gas_constant_f32;
+extern const F64 gas_constant_f64;
+
+extern const F32 refractive_index_air_f32;
+extern const F64 refractive_index_air_f64;
+
 #endif
